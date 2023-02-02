b3426129813d0252f19b359ccc5ffbcc3e2f6be0
saintfish <yusheng.sjtu@gmail.com>
2023-02-01 16:31:41 -0800

    Compress the outputs
    And we don't need to check in compressed svg anymore

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import yaml from '@rollup/plugin-yaml';
+import compress from "astro-compress";
 
-
+// https://astro.build/config
 export default defineConfig({
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), tailwind()],
+  }), tailwind(), compress()],
   vite: {
     plugins: [yaml()]
   }
