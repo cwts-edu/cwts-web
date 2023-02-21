f288c3d0942978e519116766c7942ec275b61c4f
saintfish <yusheng.sjtu@gmail.com>
2023-02-20 18:09:28 -0800

    Implement Carousel with Swiper

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,13 @@ import yaml from '@rollup/plugin-yaml';
 import compress from "astro-compress";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), tailwind(), compress()],
+  }), tailwind(), compress(), react()],
   vite: {
     plugins: [yaml()]
   }
---
+++ src/components/home/Carousel.tsx
@@ -0,0 +1,39 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+interface CarouselItem {
+  image: string;
+  link: String;
+}
+
+export interface Props {
+  items: CarouselItem[];
+}
+
+export default function Carousel(props: Props) {
+  return (
+    <Swiper
+      navigation={true}
+      pagination={{
+        clickable: true,
+      }}
+      loop={true}
+      autoplay={{
+        delay: 5000,
+      }}
+      modules={[Autoplay, Navigation, Pagination]}
+    >
+      {props.items.map((item, index) => (
+        <SwiperSlide key={index}>
+          <a href={item.link.toString()}>
+            <img src={item.image} className={"w-full"} />
+          </a>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
