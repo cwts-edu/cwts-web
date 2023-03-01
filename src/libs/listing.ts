582417b12603d474005aa216436a023150c9bf26
saintfish <yusheng.sjtu@gmail.com>
2023-02-28 23:48:32 -0800

    Add section listing (unstyled yet)

---
+++ src/libs/listing.ts
@@ -0,0 +1,29 @@
+import { getCollection, getEntryBySlug, CollectionEntry } from "astro:content";
+
+export interface ListingItem {
+  url: string;
+  thumbnail: string;
+  title: string;
+}
+
+export default async function listChildren(
+  slug: string
+): Promise<ListingItem[]> {
+  const descendants = await getCollection(
+    "pages",
+    (page) => page.slug.startsWith(slug) && page.slug !== slug
+  );
+
+  const children = descendants
+    .filter((page) => {
+      const relPath = page.slug.slice(slug.length + 1);
+      return !relPath.includes("/");
+    })
+    .sort((a, b) => a.data.order - b.data.order);
+
+  return children.map((page) => ({
+    url: "/" + page.slug,
+    thumbnail: page.data.thumbnail || "/images/default.thumbnail.jpg",
+    title: page.data.title,
+  }));
+}
