eee2776e979a5534c9e37e700e2633746e0e9f7c
saintfish <yusheng.sjtu@gmail.com>
2023-02-28 23:15:42 -0800

    Tune default section page style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,14 @@
 const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, "$1")
+    .replace(/\.0$/, "");
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
+
 const defaultFont = [
   "Helvetica",
   "Tahoma",
@@ -19,6 +27,14 @@ const defaultFont = [
   "sans-serif",
 ];
 
+const themeColors = {
+  darkerviolet: "#330047",
+  darkviolet: "#410659",
+  maxpurple: "#6E4080",
+  rebeccapurple: "#6126A2",
+  darkblue: "#211F54",
+};
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -26,21 +42,48 @@ module.exports = {
     extend: {
       colors: {
         darkerviolet: {
-          DEFAULT: "#330047",
+          DEFAULT: themeColors.darkerviolet,
         },
         darkviolet: {
-          DEFAULT: "#410659",
+          DEFAULT: themeColors.darkviolet,
         },
         maxpurple: {
-          DEFAULT: "#6E4080",
+          DEFAULT: themeColors.maxpurple,
         },
         rebeccapurple: {
-          DEFAULT: "#6126A2",
+          DEFAULT: themeColors.rebeccapurple,
         },
         darkblue: {
-          DEFAULT: "#211F54",
+          DEFAULT: themeColors.darkblue,
         },
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": colors.black,
+            "--tw-prose-headings": themeColors.darkviolet,
+            "--tw-prose-lead": themeColors.darkblue,
+            fontSize: rem(20),
+            lineHeight: round(28 / 20),
+            h1: {
+              fontSize: em(30, 30),
+              marginTop: em(30, 30),
+              marginBottom: em(30, 30),
+            },
+            h2: {
+              fontSize: em(28, 20),
+              marginTop: em(28, 28),
+              marginBottom: em(28, 28),
+            },
+            '[class~="lead"]': {
+              fontSize: em(20, 20),
+              lineHeight: round(28 / 20),
+              marginTop: em(20, 20),
+              marginBottom: em(20, 20),
+            },
+          },
+        },
+      }),
     },
     fontFamily: {
       sans: defaultFont,
