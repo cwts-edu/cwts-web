15c4fa4f8717b1867934b6b8bd19457b7f9f9d60
saintfish <yusheng.sjtu@gmail.com>
2023-09-07 21:43:54 -0700

    Add an option to open carousel links in new window

--- src/components/common/OptionalLink.tsx
+++ src/components/common/OptionalLink.tsx
@@ -3,16 +3,25 @@ import type { ReactNode } from "react";
 
 interface OptionalLinkProps {
   url?: string;
+  newWindow?: boolean;
   children: ReactNode;
 }
 
-const OptionalLink: React.FC<OptionalLinkProps> = ({ url, children }) => {
+const OptionalLink: React.FC<OptionalLinkProps> = ({
+  url,
+  newWindow = false,
+  children,
+}) => {
   if (url === undefined) {
     // If url is not provided, render the children as is
     return <>{children}</>;
   } else {
     // If url is provided, wrap the children in an anchor element
-    return <a href={url}>{children}</a>;
+    return (
+      <a href={url} target={newWindow ? "_blank" : undefined}>
+        {children}
+      </a>
+    );
   }
 };
 
