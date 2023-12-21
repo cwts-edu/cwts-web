26aff7fcda143d7e98888f25a4a417a216f01ef0
martian523 <jane.chang@cwts.edu>
2023-12-21 09:26:54 -0800

    upload banner & youtube video
    - Carousel banner
    - homepage video
    - update wording on diploma

--- src/libs/site.ts
+++ src/libs/site.ts
@@ -11,7 +11,7 @@ const siteData = {
     title: "CWTS 基督工人神學院",
     shortTitle: "基督工人神學院",
     home: "/",
-    coverVideoTitle: "基督工人神學院 50週年回顧與展望",
+    coverVideoTitle: "基督工人神學院 CWTS Merry Christmas 2023",
   },
 };
 
@@ -27,7 +27,7 @@ class Site {
   }
   defaultCover = "/images/covers/default.cover.jpg";
   defaultThumbnail = "/images/covers/default.thumbnail.jpg";
-  coverVideoId = "TZGecfDvtAs";
+  coverVideoId = "LlEvCTU0UG8";
   coverVideoTitle(language: Language): string {
     return siteData[language].coverVideoTitle;
   }
