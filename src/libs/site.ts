c647149bce51caad942b87b0e759bb83662a807e
Boonhub <138715895+Boonhub@users.noreply.github.com>
2024-01-06 11:34:57 -0800

    Replace-Home-Page-Video (#113)
    * Replace-Home-Page-Video
    
    Replace HOME page youtube video
    
    ---------
    
    Co-authored-by: saintfish <yusheng.sjtu@gmail.com>

--- src/libs/site.ts
+++ src/libs/site.ts
@@ -5,13 +5,13 @@ const siteData = {
     title: "Christian Witness Theological Seminary",
     shortTitle: "CWTS",
     home: "/en",
-    coverVideoTitle: "Christian Witness Theological Seminary CWTS Merry Christmas 2023",
+    coverVideoTitle: "A seminarian's testimony: How theological studies have uplifted and transformed me",
   },
   zh: {
     title: "CWTS 基督工人神學院",
     shortTitle: "基督工人神學院",
     home: "/",
-    coverVideoTitle: "基督工人神學院 CWTS Merry Christmas 2023",
+    coverVideoTitle: "神學生見證 - 神學學習如何提升和改變我",
   },
 };
 
@@ -27,7 +27,7 @@ class Site {
   }
   defaultCover = "/images/covers/default.cover.jpg";
   defaultThumbnail = "/images/covers/default.thumbnail.jpg";
-  coverVideoId = "LlEvCTU0UG8";
+  coverVideoId = "fkfx1alIKrI";
   coverVideoTitle(language: Language): string {
     return siteData[language].coverVideoTitle;
   }
