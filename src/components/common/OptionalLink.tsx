efbffdfe1ceba1aead2106a269936da91c274d0b
susansxdgit <susansongxiaodong@gmail.com>
2025-03-25 22:15:44 -0700

    Highlight when mouse hover over menus
    Add prop className to component OptionalLink
    
    add class "hover:underline hover:font-bold transition duration-300" to highlight menu item with under line and bold

--- src/components/common/OptionalLink.tsx
+++ src/components/common/OptionalLink.tsx
@@ -5,23 +5,37 @@ interface OptionalLinkProps {
   url?: string;
   newWindow?: boolean;
   children: ReactNode;
+  className?: string;
 }
 
 const OptionalLink: React.FC<OptionalLinkProps> = ({
   url,
   newWindow = false,
   children,
+  className,
 }) => {
   if (url === undefined) {
     // If url is not provided, render the children as is
     return <>{children}</>;
   } else {
-    // If url is provided, wrap the children in an anchor element
-    return (
-      <a href={url} target={newWindow ? "_blank" : undefined}>
-        {children}
-      </a>
-    );
+    if (className === undefined) {
+          // If url is provided, wrap the children in an anchor element
+      return (
+        <a href={url} target={newWindow ? "_blank" : undefined}>
+         {children}
+        </a>
+      );
+    } else {
+
+          // If url is provided, wrap the children in an anchor element
+      return (
+        <a href={url} className={className} target={newWindow ? "_blank" : undefined}>
+          {children}
+        </a>
+      );
+    }
+
+
   }
 };
 
