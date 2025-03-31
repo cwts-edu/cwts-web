01884069217f4324a0155a72d3c1a993cb4299f7
susansxdgit <susansongxiaodong@gmail.com>
2025-03-31 14:57:19 -0700

    add back comments

--- src/components/common/OptionalLink.tsx
+++ src/components/common/OptionalLink.tsx
@@ -18,12 +18,13 @@ const OptionalLink: React.FC<OptionalLinkProps> = ({
     // If url is not provided, render the children as is
     return <>{children}</>;
   } else {
+    // If url is provided, wrap the children in an anchor element
     return (
-        <a href={url} className={className} target={newWindow ? "_blank" : undefined}>
-          {children}
-        </a>
-      );
-    }
+      <a href={url} className={className} target={newWindow ? "_blank" : undefined}>
+        {children}
+      </a>
+    );
+  }
 };
 
 export default OptionalLink;
