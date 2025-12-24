0993bbde182cde7a8ee6841cc992d67309cfda78
saintfish <yusheng.sjtu@gmail.com>
2025-12-24 00:38:54 -0600

    Open first degree in each group
    Set accordion names explicitly. Context didn't work. context are shared between components

--- src/context/accordion.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from '@astropub/context'
-
-export const [AccordionProvider, getAccordionContext] = createContext<{
-  name: string;
-  summaryClassNames?: string[];
-  detailsClassNames?: string[];
-}>();
